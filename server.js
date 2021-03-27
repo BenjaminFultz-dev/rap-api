3a14c7fd9cfae9ef0f28fd089ab32e0f4a9ad165
Benjamin Fultz <xsymbiotex@gmail.com>
2021-03-27 13:57:32 -0400

    yeast

--- server.js
+++ server.js
@@ -252,6 +252,11 @@ let rappers = {
         'birthName': 'David R. Styles',
         'birthLocation': 'New York, New York'
     },
+    'chuck d': {
+        'age': 60,
+        'birthName': 'Carlton Douglas Ridenhour',
+        'birthLocation': 'New York, New York'
+    },
 }
 
 
