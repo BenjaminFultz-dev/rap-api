3240113d3540ec8eaf17d34b7c6f01e8206bae30
Benjamin Fultz <xsymbiotex@gmail.com>
2021-03-31 14:15:48 -0400

    naughty by nature

--- server.js
+++ server.js
@@ -277,6 +277,11 @@ let rappers = {
         'birthName': 'Anthony Criss',
         'birthLocation': 'New York, New York'
     },
+    'chuck d': {
+        'age': 60,
+        'birthName': 'Carlton Douglas Ridenhour',
+        'birthLocation': 'New York, New York'
+    },
 }
 
 
