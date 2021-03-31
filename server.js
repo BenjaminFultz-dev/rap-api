5b11fcf3ac925a7d7fbe91760fca39c0e7eeaa43
Benjamin Fultz <xsymbiotex@gmail.com>
2021-03-31 13:53:35 -0400

    east still poppin

--- server.js
+++ server.js
@@ -242,6 +242,11 @@ let rappers = {
         'birthName': 'Jason Terrance Phillips',
         'birthLocation': 'New York, New York'
     },
+    'chuck d': {
+        'age': 60,
+        'birthName': 'Carlton Douglas Ridenhour',
+        'birthLocation': 'New York, New York'
+    },
 }
 
 
