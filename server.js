e46f607b785c9746d8da0f53410aa76f0eabd6ed
Benjamin Fultz <xsymbiotex@gmail.com>
2021-04-04 14:19:50 -0400

    continuing east

--- server.js
+++ server.js
@@ -287,6 +287,11 @@ let rappers = {
         'birthName': 'Keith Edward Elam',
         'birthLocation': 'New York, New York'
     },
+    'chuck d': {
+        'age': 60,
+        'birthName': 'Carlton Douglas Ridenhour',
+        'birthLocation': 'New York, New York'
+    },
 }
 
 
