4407844d9acc91c150d0febb25303f52d4021b96
Benjamin Fultz <xsymbiotex@gmail.com>
2021-04-16 13:32:44 -0400

    adding more

--- server.js
+++ server.js
@@ -197,6 +197,11 @@ let rappers = {
         'birthName': 'Curtis James Jackson III',
         'birthLocation': 'New York, New York'
     },
+    'chuck d': {
+        'age': 60,
+        'birthName': 'Carlton Douglas Ridenhour',
+        'birthLocation': 'New York, New York'
+    },
 }
 
 
