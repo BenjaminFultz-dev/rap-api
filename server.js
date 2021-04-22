1dc8843b7e2d16d73a9a0eca89cc543ead15b9fc
Benjamin Fultz <xsymbiotex@gmail.com>
2021-04-22 14:39:18 -0400

    this api getting thick

--- server.js
+++ server.js
@@ -337,6 +337,11 @@ let rappers = {
         'birthName': 'William Hines',
         'birthLocation': 'New York, New York'
     },
+    'chuck d': {
+        'age': 60,
+        'birthName': 'Carlton Douglas Ridenhour',
+        'birthLocation': 'New York, New York'
+    },
 }
 
 
