63af725f33870d35a5a65c637a171b3e42c997ea
Benjamin Fultz <xsymbiotex@gmail.com>
2021-04-24 13:16:33 -0400

    east coast

--- server.js
+++ server.js
@@ -177,6 +177,11 @@ let rappers = {
         'birthName': 'Percy Carey',
         'birthLocation': 'New York, New York'
     },
+    'chuck d': {
+        'age': 60,
+        'birthName': 'Carlton Douglas Ridenhour',
+        'birthLocation': 'New York, New York'
+    },
 }
 
 
