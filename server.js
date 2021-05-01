7370a2a1a192e7218092cb74b48129b0f637af3f
Benjamin Fultz <xsymbiotex@gmail.com>
2021-05-01 15:01:27 -0400

    fugees

--- server.js
+++ server.js
@@ -367,6 +367,11 @@ let rappers = {
         'birthName': 'Lauryn Noelle Hill',
         'birthLocation': 'New York, New York'
     },
+    'chuck d': {
+        'age': 60,
+        'birthName': 'Carlton Douglas Ridenhour',
+        'birthLocation': 'New York, New York'
+    },
 }   
 
 
