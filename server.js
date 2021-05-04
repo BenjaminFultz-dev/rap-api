80f955bda2b6d4cebb811378b0684aa88bcf0675
Benjamin Fultz <xsymbiotex@gmail.com>
2021-05-04 13:06:56 -0400

    east

--- server.js
+++ server.js
@@ -152,6 +152,11 @@ let rappers = {
         'birthName': 'Richard Martin Lloyd Walters',
         'birthLocation': 'New York, New York'
     },
+    'chuck d': {
+        'age': 60,
+        'birthName': 'Carlton Douglas Ridenhour',
+        'birthLocation': 'New York, New York'
+    },
 }
 
 
