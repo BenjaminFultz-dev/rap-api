34eaf77f8c18923d342f9cb5793b7dbb74335171
Benjamin Fultz <xsymbiotex@gmail.com>
2021-05-08 15:11:16 -0400

    next up

--- server.js
+++ server.js
@@ -387,6 +387,11 @@ let rappers = {
         'birthName': 'Marcel Theo Hall',
         'birthLocation': 'New York, New York'
     },
+    'chuck d': {
+        'age': 60,
+        'birthName': 'Carlton Douglas Ridenhour',
+        'birthLocation': 'New York, New York'
+    },
 }   
 
 
