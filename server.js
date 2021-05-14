17cbc15db9bef1d99af834b84a73554c17929347
Benjamin Fultz <xsymbiotex@gmail.com>
2021-05-14 15:17:32 -0400

    pushing east

--- server.js
+++ server.js
@@ -402,6 +402,11 @@ let rappers = {
         'birthName': 'Lorenzo Dechalus',
         'birthLocation': 'New York, New York'
     },
+    'chuck d': {
+        'age': 60,
+        'birthName': 'Carlton Douglas Ridenhour',
+        'birthLocation': 'New York, New York'
+    },
 }   
 
 
