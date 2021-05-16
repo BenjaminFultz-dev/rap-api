167a809121ddb011787c260fb0e0f5c30fa75bbe
Benjamin Fultz <xsymbiotex@gmail.com>
2021-05-16 15:20:11 -0400

    moving to detroit

--- server.js
+++ server.js
@@ -407,6 +407,11 @@ let rappers = {
         'birthName': 'Walter Reed',
         'birthLocation': 'New York, New York'
     },
+    'chuck d': {
+        'age': 60,
+        'birthName': 'Carlton Douglas Ridenhour',
+        'birthLocation': 'New York, New York'
+    },
 }   
 
 
