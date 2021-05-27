7786c1d360497236094006bb9f2da707f592cea1
Benjamin Fultz <xsymbiotex@gmail.com>
2021-05-27 15:53:07 -0400

    cha cha

--- server.js
+++ server.js
@@ -432,9 +432,9 @@ let rappers = {
         'birthName': 'Daniel Dewan Sewell',
         'birthLocation': 'Detroit, Michigan'
     },
-    'big sean': {
-        'age': 33,
-        'birthName': 'Sean Michael-Leonard Anderson',
+    'cha cha': {
+        'age': 40,
+        'birthName': 'Parris Lynell Fluellen',
         'birthLocation': 'Detroit, Michigan'
     },
 }   
