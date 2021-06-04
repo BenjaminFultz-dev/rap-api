992bca212e7ba564bbe4878841df34941f3c5eeb
Benjamin Fultz <xsymbiotex@gmail.com>
2021-06-04 16:04:37 -0400

    committing

--- server.js
+++ server.js
@@ -457,6 +457,11 @@ let rappers = {
         'birthName': 'Shabazz Ford',
         'birthLocation': 'Detroit, Michigan'
     },
+    'big sean': {
+        'age': 33,
+        'birthName': 'Sean Michael-Leonard Anderson',
+        'birthLocation': 'Detroit, Michigan'
+    },
 }   
 
 
