ef1994635e0410f08293f9b8a5f28d4e67d02c3f
Benjamin Fultz <xsymbiotex@gmail.com>
2021-06-06 16:12:50 -0400

    detroit continues

--- server.js
+++ server.js
@@ -462,6 +462,11 @@ let rappers = {
         'birthName': 'Trish Best',
         'birthLocation': 'Detroit, Michigan'
     },
+    'big sean': {
+        'age': 33,
+        'birthName': 'Sean Michael-Leonard Anderson',
+        'birthLocation': 'Detroit, Michigan'
+    },
 }   
 
 
