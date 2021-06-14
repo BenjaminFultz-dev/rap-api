bf4361583daf74db40e6710d916051b3f6528a8b
Benjamin Fultz <xsymbiotex@gmail.com>
2021-06-14 16:21:34 -0400

    committing more detroit rappers

--- server.js
+++ server.js
@@ -482,6 +482,11 @@ let rappers = {
         'birthName': 'Raykeea Raeen-Roes Wilson',
         'birthLocation': 'Detroit, Michigan'
     },
+    'big sean': {
+        'age': 33,
+        'birthName': 'Sean Michael-Leonard Anderson',
+        'birthLocation': 'Detroit, Michigan'
+    },
 }   
 
 
