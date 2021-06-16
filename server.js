500b699ba94ea18dbff38399aecad798ac38495a
Benjamin Fultz <xsymbiotex@gmail.com>
2021-06-16 16:23:30 -0400

    detroit continued

--- server.js
+++ server.js
@@ -487,6 +487,11 @@ let rappers = {
         'birthName': 'Daniel Carlisle',
         'birthLocation': 'Detroit, Michigan'
     },
+    'big sean': {
+        'age': 33,
+        'birthName': 'Sean Michael-Leonard Anderson',
+        'birthLocation': 'Detroit, Michigan'
+    },
 }   
 
 
