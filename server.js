20936448b3c7f420d7f88e35ad6fe15e7b5732f8
Benjamin Fultz <xsymbiotex@gmail.com>
2021-07-08 16:31:01 -0400

    works

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = 8000;
+
+app.use(cors());
+
+let rappers = {
+    '21 savage': {
+        'age': 28,
+        'birthName': 'Sheyaa Bin Abraham-Joseph',
+        'birthLocation': 'London, England'
+    },
+    'chance the rapper': {
+        'age': 27,
+        'birthName': 'Chancellor Jonathan Bennett',
+        'birthLocation': 'Chicago, Illinois'
+    },
+    'dylan': {
+        'age': 26,
+        'birthName': 'Dylan',
+        'birthLocation': 'Dylan'
+    }
+}
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+});
+
+app.get('/api/rappers/:rapperName', (req, res) => {
+    const rapName = req.params.rapperName.toLowerCase()
+    if (rappers[rapName]) {
+        res.json(rappers[rapName])  
+    } else {
+        res.json(rappers['dylan'])   
+    }
+    
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`)
+});
