1b4dca6244ca384a5778d4d8ed519bd6b78e7dab
Benjamin Fultz <xsymbiotex@gmail.com>
2021-07-11 16:46:05 -0400

    heading south for the trap

--- server.js
+++ server.js
@@ -542,6 +542,11 @@ let rappers = {
         'birthName': 'Alvin Nathaniel Joiner',
         'birthLocation': 'Detroit, Michigan'
     },
+    'big sean': {
+        'age': 33,
+        'birthName': 'Sean Michael-Leonard Anderson',
+        'birthLocation': 'Detroit, Michigan'
+    },
 }   
 
 
